94c95f25db7632f84369325e6c144cd63d6fd8d8
winston <winstonfeng0730@gmail.com>
2026-08-05 20:53:45 -0700

    Update BcryptPasswordHasher.js

--- server/src/infrastructure/security/BcryptPasswordHasher.js
+++ server/src/infrastructure/security/BcryptPasswordHasher.js
@@ -1,7 +1,15 @@
 import bcrypt from 'bcryptjs';
 
+/**
+ * PasswordHasher port, backed by bcrypt.
+ *
+ * The cost factor is injected rather than hard-coded: tests want it low so they
+ * run fast, production wants it high enough to be slow on purpose.
+ *
+ * @implements {import('../../domain/ports.js').PasswordHasher}
+ */
 export class BcryptPasswordHasher {
-  constructor(rounds = 10) {
+  constructor({ rounds = 10 } = {}) {
     this.rounds = rounds;
   }
 
